d1797c1bd2323335a6ad42b0d6461c624870d172
libnihernandez02 <62312180+libnihernandez02@users.noreply.github.com>
2020-06-15 15:59:50 -0600

    Add files via upload

---
+++ Controlador/Home.Controller.php
@@ -0,0 +1,20 @@
+<?php
+  /**
+   * Clase home que permite la primera carga del sistema
+   */
+  class Home
+  {
+
+    function __construct()
+    {
+      // code...
+    }
+
+    public function Inicio()
+    {
+        echo "En clase Home y metodo inicio";
+    }
+
+  }
+
+ ?>
