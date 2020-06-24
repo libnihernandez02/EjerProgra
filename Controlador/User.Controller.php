6287a9cb4f441f073299749d9d85d39077d37e5c
Libni Andrea Hernández Rosales <201900040@colsanjose.edu.gt>
2020-06-24 13:45:00 -0600

    Fin

--- Controlador/User.Controller.php
+++ Controlador/User.Controller.php
@@ -7,19 +7,37 @@ class User
 	
 	}
 
+	
 	public function ValidarUsuario()
 	{
+				
 		//echo "en controller user y metodo  ValidarUsuario<br>";
 		//var_dump($_POST);
 		$user=new Usuarios(); 
+		$smarty=new Smarty();
+		
 
 		$usuario=$_POST['usuario'];
 		$pass=$_POST['password'];
 		$tipo=$_POST['tipo'];
 
 		$dato=$user->BuscarUsuario($usuario, $pass, $tipo);
+		if($dato->num_rows==1)
+		{
+			$ar=array();
 
-		var_dump($dato);
+        while($row=mysqli_fetch_assoc($dato))
+        {
+            echo $row['user']."---".$row['tipo'];
+        }
+
+			
+		}
+		else
+		{
+		
+		}
+		
 	}
 }
 
